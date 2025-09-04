ad26a8b35ae4175004d686fd5ebd601356be9c0b
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-09-04 15:07:26 +0200

    Typo on ports

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,7 @@ const getDefaultApiUrl = (): string => {
   if (process.env.NODE_ENV === 'production') {
     return process.env.NEXT_PUBLIC_API_URL || 'https://api.cvenom.com/api';
   }
-  return process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+  return process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:4002/api';
 };
 
 export const getApiUrl = (): string => {
