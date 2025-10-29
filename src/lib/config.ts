95a8101c053f9ac0d8877524957bb7d36892025c
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-10-29 21:47:29 +0100

    Fix default backend url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,7 +7,7 @@
 
 const getDefaultApiUrl = (): string => {
   if (process.env.NODE_ENV === 'production') {
-    return process.env.NEXT_PUBLIC_API_URL || 'https://gateway.api0.ai/api';
+    return process.env.NEXT_PUBLIC_API_URL || 'https://api.cvenom.com/api';
   }
   return process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:4002/api';
 };
