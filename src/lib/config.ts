5475700fa8c2c3cf4947c5c2ba79df4ebee3edab
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-10-29 22:13:14 +0100

    Clean

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,3 @@
-// const getDefaultApiUrl = (): string => {
-//   if (process.env.NODE_ENV === 'production') {
-//     return process.env.NEXT_PUBLIC_API_URL || 'https://api.cvenom.com/api';
-//   }
-//   return process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:4002/api';
-// };
-
 const getDefaultApiUrl = (): string => {
   if (process.env.NODE_ENV === 'production') {
     return process.env.NEXT_PUBLIC_API_URL || 'https://api.cvenom.com/api';
