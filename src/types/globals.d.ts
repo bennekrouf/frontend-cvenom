a3dc158369f0eb8ffadfc9499ea7a58524843956
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-11-03 10:33:12 +0100

    Typo

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,8 +1,7 @@
-export {};
+export { };
 
 declare global {
   interface Window {
-    // Add your global window types here
     plausible?: {
       (event: string, options?: { props?: Record<string, string> }): void;
       q?: Array<unknown>;
