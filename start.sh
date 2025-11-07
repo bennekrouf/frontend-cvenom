2048b3145ee18b63bb464a7f66f90ed64e8b3f37
bennekrouf <mohamed.bennekrouf@gmail.com>
2025-11-07 20:40:37 +0000

    Add api key

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /opt/cvenom/frontend
 
+export NEXT_PUBLIC_API0_API_KEY=sk_live_5hXe-HDNPstg2bnUAEP9S8GIv5J4G36M
 # Pass through all environment variables from PM2
 exec node_modules/.bin/next start
